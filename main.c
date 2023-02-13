8a6a946b9a9dbd1965255f75d0ac889b1128f104
Jonathan <92918075+jonathanplsmith@users.noreply.github.com>
2023-02-13 21:32:58 +0100

    reworked main

--- main.c
+++ main.c
@@ -7,23 +7,23 @@
 
 int main(void) {
 
-    canvas_t *cons = newCanvas(21, 24);
+    canvas_t *cons = newCanvas(21, 48);
 
     setFg(cons, GREEN);
     setBg(cons, BLACK);
-    placeTextHor(cons, "You can write colourful text to the console", 2, 3);
+    placeTextHor(cons, "You can write colourful text to the console", 2, 6);
     refreshConsole(cons);
     sleep(1);
     setFg(cons, WHITE);
     setBg(cons, RED);
-    placeTextVert(cons, "Vertically too!", 4, 5);
+    placeTextVert(cons, "Vertically too!", 4, 10);
     refreshConsole(cons);
     sleep(3);
     setFg(cons, RESET_COLOUR);
     setBg(cons, RESET_COLOUR);
 
     clearCanvas(cons);
-    placeTextHor(cons, "You can draw characters to the console", 2, 5);
+    placeTextHor(cons, "You can draw characters to the console", 2, 10);
     refreshConsole(cons);
     setFg(cons, BYELLOW);
     sleep(1);
@@ -49,40 +49,40 @@ int main(void) {
     }
     setFg(cons, RESET_COLOUR);
     clearCanvas(cons);
-    placeTextHor(cons, "Or you could draw some lines", 2, 5);
+    placeTextHor(cons, "Or you could draw some lines", 2, 10);
     refreshConsole(cons);
     sleep(1);
 
     clearCanvas(cons);
     setFg(cons, BBLUE);
-    drawLine(cons, '#', 0, 11, 9, 0);
+    drawLine(cons, '#', 0, 22, 9, 0);
     refreshConsole(cons);
     usleep(10 * DELAY);
     setFg(cons, BRED);
-    drawLine(cons, '#', 9, 0, 20, 9);
+    drawLine(cons, '#', 9, 0, 20, 18);
     refreshConsole(cons);
     usleep(10 * DELAY);
     setFg(cons, BGREEN);
-    drawLine(cons, '#', 20, 9, 11, 20);
+    drawLine(cons, '#', 20, 18, 11, 40);
     refreshConsole(cons);
     usleep(10 * DELAY);
     setFg(cons, BYELLOW);
-    drawLine(cons, '#', 0, 11, 11, 20);
+    drawLine(cons, '#', 0, 22, 11, 40);
     refreshConsole(cons);
     usleep(10 * DELAY);
     setFg(cons, WHITE);
-    drawLine(cons, 'X', 0, 0, 20, 20);
+    drawLine(cons, 'X', 0, 0, 20, 40);
     refreshConsole(cons);
     usleep(10 * DELAY);
     setFg(cons, WHITE);
-    drawLine(cons, 'X', 20, 0, 0, 20);
+    drawLine(cons, 'X', 20, 0, 0, 40);
     setFg(cons, RESET_COLOUR);
     setBg(cons, RESET_COLOUR);
     refreshConsole(cons);
     sleep(1);
 
     clearCanvas(cons);
-    placeTextHor(cons, "Or how about a sprite?", 2, 5);
+    placeTextHor(cons, "Or how about a sprite?", 2, 10);
     refreshConsole(cons);
     sleep(1);
 
@@ -96,15 +96,15 @@ int main(void) {
     };
 
     clearCanvas(cons);
-    drawSprite(cons, smiley, ROW, COL, 10, 10);
+    drawSprite(cons, smiley, ROW, COL, 10, 20);
     refreshConsole(cons);
     usleep(25 * DELAY);
     clearCanvas(cons);
-    drawSprite(cons, smiley, ROW, COL, 2, 4);
+    drawSprite(cons, smiley, ROW, COL, 2, 8);
     refreshConsole(cons);
     usleep(25 * DELAY);
     clearCanvas(cons);
-    drawSprite(cons, smiley, ROW, COL, 15, 9);
+    drawSprite(cons, smiley, ROW, COL, 15, 18);
     refreshConsole(cons);
 
     deleteCanvas(cons);
