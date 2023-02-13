13dd6b43b38293d1700edeff669a6599c245d468
Jonathan <92918075+jonathanplsmith@users.noreply.github.com>
2023-02-13 22:20:40 +0100

    final (hopefully)

--- main.c
+++ main.c
@@ -9,49 +9,49 @@ int main(void) {
 
     canvas_t *cons = newCanvas(21, 47);
 
-    // setFg(cons, GREEN);
-    // setBg(cons, BLACK);
-    // placeTextHor(cons, "You can write colourful text to the console", 2, 3);
-    // refreshConsole(cons);
-    // sleep(1);
-    // setFg(cons, WHITE);
-    // setBg(cons, RED);
-    // placeTextVert(cons, "Vertically too!", 4, 5);
-    // refreshConsole(cons);
-    // sleep(3);
-    // setFg(cons, RESET_COLOUR);
-    // setBg(cons, RESET_COLOUR);
+    setFg(cons, GREEN);
+    setBg(cons, BLACK);
+    placeTextHor(cons, "You can write colourful text to the console", 2, 3);
+    refreshConsole(cons);
+    sleep(1);
+    setFg(cons, WHITE);
+    setBg(cons, RED);
+    placeTextVert(cons, "Vertically too!", 4, 5);
+    refreshConsole(cons);
+    sleep(3);
+    setFg(cons, RESET_COLOUR);
+    setBg(cons, RESET_COLOUR);
 
-    // clearCanvas(cons);
-    // placeTextHor(cons, "You can draw characters to the console", 2, 5);
-    // refreshConsole(cons);
-    // setFg(cons, BYELLOW);
-    // sleep(1);
-    // for (int i=0; i<cons->maxX; i++) {
-    //     inputChar(cons, '*', i, 0);
-    //     refreshConsole(cons);
-    //     usleep(DELAY);
-    // }
-    // for (int i=0; i<cons->maxY; i+=2) {
-    //     inputChar(cons, '*', cons->maxX-1, i);
-    //     refreshConsole(cons);
-    //     usleep(DELAY); 
-    // }
-    // for (int i=cons->maxX-1; i>=0; i--) {
-    //     inputChar(cons, '*', i, cons->maxY-1);
-    //     refreshConsole(cons);
-    //     usleep(DELAY);
-    // }
-    // for (int i=cons->maxY-1; i>=0; i-=2) {
-    //     inputChar(cons, '*', 0, i);
-    //     refreshConsole(cons);
-    //     usleep(DELAY);
-    // }
-    // setFg(cons, RESET_COLOUR);
-    // clearCanvas(cons);
-    // placeTextHor(cons, "Or you could draw some lines", 2, 5);
-    // refreshConsole(cons);
-    // sleep(1);
+    clearCanvas(cons);
+    placeTextHor(cons, "You can draw characters to the console", 2, 5);
+    refreshConsole(cons);
+    setFg(cons, BYELLOW);
+    sleep(1);
+    for (int i=0; i<cons->maxX; i++) {
+        inputChar(cons, '*', i, 0);
+        refreshConsole(cons);
+        usleep(DELAY);
+    }
+    for (int i=0; i<cons->maxY; i+=2) {
+        inputChar(cons, '*', cons->maxX-1, i);
+        refreshConsole(cons);
+        usleep(DELAY); 
+    }
+    for (int i=cons->maxX-1; i>=0; i--) {
+        inputChar(cons, '*', i, cons->maxY-1);
+        refreshConsole(cons);
+        usleep(DELAY);
+    }
+    for (int i=cons->maxY-1; i>=0; i-=2) {
+        inputChar(cons, '*', 0, i);
+        refreshConsole(cons);
+        usleep(DELAY);
+    }
+    setFg(cons, RESET_COLOUR);
+    clearCanvas(cons);
+    placeTextHor(cons, "Or you could draw some lines", 2, 5);
+    refreshConsole(cons);
+    sleep(1);
 
     clearCanvas(cons);
     setFg(cons, BBLUE);
@@ -81,31 +81,31 @@ int main(void) {
     refreshConsole(cons);
     sleep(1);
 
-    // clearCanvas(cons);
-    // placeTextHor(cons, "Or how about a sprite?", 2, 10);
-    // refreshConsole(cons);
-    // sleep(1);
+    clearCanvas(cons);
+    placeTextHor(cons, "Or how about a sprite?", 2, 10);
+    refreshConsole(cons);
+    sleep(1);
 
-    // #define ROW 4
-    // #define COL 5
-    // char smiley [ROW * COL] = {
-    //     ' ', '|', ' ', '|', ' ',
-    //     ' ', '|', ' ', '|', ' ',
-    //     ' ', ' ', ' ', ' ', ' ',
-    //     '\\', '_', '_', '_', '/'
-    // };
+    #define ROW 4
+    #define COL 5
+    char smiley [ROW * COL] = {
+        ' ', '|', ' ', '|', ' ',
+        ' ', '|', ' ', '|', ' ',
+        ' ', ' ', ' ', ' ', ' ',
+        '\\', '_', '_', '_', '/'
+    };
 
-    // clearCanvas(cons);
-    // drawSprite(cons, smiley, ROW, COL, 10, 20);
-    // refreshConsole(cons);
-    // usleep(25 * DELAY);
-    // clearCanvas(cons);
-    // drawSprite(cons, smiley, ROW, COL, 2, 8);
-    // refreshConsole(cons);
-    // usleep(25 * DELAY);
-    // clearCanvas(cons);
-    // drawSprite(cons, smiley, ROW, COL, 15, 18);
-    // refreshConsole(cons);
+    clearCanvas(cons);
+    drawSprite(cons, smiley, ROW, COL, 10, 20);
+    refreshConsole(cons);
+    usleep(25 * DELAY);
+    clearCanvas(cons);
+    drawSprite(cons, smiley, ROW, COL, 2, 8);
+    refreshConsole(cons);
+    usleep(25 * DELAY);
+    clearCanvas(cons);
+    drawSprite(cons, smiley, ROW, COL, 15, 18);
+    refreshConsole(cons);
 
     deleteCanvas(cons);
     
